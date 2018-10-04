bea480ef818e1d44f16fc137f12fcdc73fc2695d
bsleon <bsleon@umich.edu>
2018-10-04 18:31:06 -0400

    fixed navbar texture bug

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -59,15 +59,59 @@
 // 	renderMoveHistory("");
 // });
 
+//****************************************************************************************
+	//set active links when scrolling
+	// $(document).ready(function () {
+	// 	var url = window.location;
+	// 	$('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+	// 	$('ul.nav a').filter(function() {
+	// 		return this.href == url;
+	// 	}).parent().addClass('active');
+	// });
 
-	//set active links
-	$(document).ready(function () {
-		var url = window.location;
-		$('ul.nav a[href="'+ url +'"]').parent().addClass('active');
-		$('ul.nav a').filter(function() {
-			return this.href == url;
-		}).parent().addClass('active');
-	});
+
+    // Basice Code keep it 
+    $(document).ready(function () {
+        $(document).on("scroll", onScroll);
+
+        //smoothscroll
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            $(document).off("scroll");
+
+            $('a').each(function () {
+                $(this).removeClass('active');
+            })
+            $(this).addClass('active');
+
+            var target = this.hash,
+                menu = target;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top+2
+            }, 500, 'swing', function () {
+                window.location.hash = target;
+                $(document).on("scroll", onScroll);
+            });
+        });
+    });
+
+// Use Your Class or ID For Selection 
+
+    function onScroll(event){
+        var scrollPos = $(document).scrollTop();
+        $('ul.nav a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('ul.nav li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    }
 
 
     //change light and dark themes
@@ -117,7 +161,9 @@
     			// $(this).css("background-color", "#93e0fb");
     			// $(this).css("border-radius", "15% 15% 0 0");
     			$("a").css("background-color", "rgb(0,0,0,0)");
+    			$("a").css("background-image", "none");
     			$(this).css("background-color", "#93e0fb");
+    			$(this).css("background-image", 'url(assets/images/texture.png)');
     			// $(this).css("border-radius", "15% 15% 0 0");
     		}
     		else{
@@ -126,7 +172,9 @@
     			// $(this).css("border-radius", "15% 15% 0 0");
     			// $(this).css("box-shadow", "inset 0 0 30px black;");
     			$("a").css("background-color", "rgb(0,0,0,0)");
+    			$("a").css("background-image", "none");
     			$(this).css("background-color", "rgb(14,32,50,.9)");
+    			$(this).css("background-image", 'url(assets/images/texture.png)');
     			// document.getElementById(this.parent).style.display = 'none';
     			// document.getElementById(this.parent).style.display = 'block';
 
