a6adb345d2907e8ac2b80725cc1cc028c9516c24
Brandon Leon <bsleon@umich.edu>
2020-07-28 15:47:36 -0400

    Update Homepage

--- ReactChessPortfolioProject/service-worker.js
+++ ReactChessPortfolioProject/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6164edb1a88deeb13df884064ab3f1d.js"
+  "/ReactChessPortfolioProject/precache-manifest.c19f2a09e15cb5425e39e64f44cbf313.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactChessPortfolioProject/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
